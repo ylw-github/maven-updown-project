0df440fef3f40e3041cde1ccd85cba997b0921fa
yanglinwei <1962264482@qq.com>
2020-02-11 11:51:29 +0800

    commit

---
+++ maven-up-project/src/main/java/com/ylw/mvn/test/User.java
@@ -0,0 +1,31 @@
+package com.ylw.mvn.test;
+
+public class User {
+    private int age;
+    private int name;
+    private int sex;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+}
